efdc2e4a7c2ea3c23e07e1d4dd59fea734f82d4a
GNVageesh <vageeshgn2005@gmail.com>
2021-08-29 19:49:59 +0530

    minor error

--- index/index1.js
+++ index/index1.js
@@ -22,7 +22,7 @@ client.on("messageCreate", (message) => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (!command.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	else if (command === "ping") {
 		client.all.get("ping").run(message);
 	}
